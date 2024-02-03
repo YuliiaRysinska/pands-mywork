674b38eea76697d39b6d6ac21dd7a485ee29697d
Админ <iuy.nickolaencko@gmail.com>
2024-02-03 22:37:31 +0000

    week02

---
+++ week02/nameAndAge.py
@@ -0,0 +1,18 @@
+# 16.create program that reads in a name and age and outputs
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print ("Hello " + name +", your age is " + age)
+
+
+# 17.modify
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print ("Hello " + name +", \tyour age is " + age)
+
+# 18.Write a program that outputs 21-4
+a = 21 - 4
+print(a)
+
+# 19. program that outputs if 2 == 3
+b = 2 == 3
+print(b)
