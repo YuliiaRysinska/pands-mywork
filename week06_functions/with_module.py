07bf7b8137c19251d4d7907b1e0cbf3a489ca3f6
Админ <iuy.nickolaencko@gmail.com>
2024-03-01 22:29:06 +0000

    main code

---
+++ week06_functions/with_module.py
@@ -0,0 +1,18 @@
+# this uses modult to breake up the code
+#main program
+
+import studentUtil as su
+
+students = []
+choice = su.displayMenu()
+while(choice != 'q'):
+ # we could do this with lamda functions
+ # I am keeping this basic for the moment
+ if choice == 'a':
+    su.doAdd(students)
+ elif choice == 'v':
+    su.doView(students)
+ elif choice !='q':
+   print("\n\nplease select either a,v or q")
+   choice = su.displayMenu()
+   
